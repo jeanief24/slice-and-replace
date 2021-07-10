5c675303ff2c231a211474d68834a92172746cbd
JeanieFNJ <jeanie@outlook.com>
2021-07-10 16:53:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let dna = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
+
+//a) Replace the gene "GCT" with "AGG", and then print the altered strand.
+dna = dna.replace('GCT','AGG');
+console.log(dna)
+
+//b) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
+//console.log (dna[40]+[41]+[42])
+console.log (dna.indexOf('CAT'));
+ if (dna[40] = 'CAT') {
+   console.log('CAT gene found');
+ } else {
+   console.log('CAT gene NOT found');
+ }
+ 
+//c) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
+
+console.log(dna.slice(16,19));
+//trim everything up to 16 and then everything after 19
+
+//d) Use a template literal to print, "The DNA strand is ___ characters long."
+console.log(`The DNA strand is ${dna.length} characters long.`)
+
+//e) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+let dna2=''
+dna2=dna.slice([40],[43])
+//console.log(dna2)
+console.log(`taco ${dna2}`)
